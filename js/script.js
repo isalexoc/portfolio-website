a5669b6d3dc685d9e4549a65aab5230b696bc99d
Isaac Orraiz <isaacorraiz@gmail.com>
2023-10-15 11:33:22 -0400

    Header section added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Add custom JavaScript here
+function runTypingEffect() {
+  const text = "I am Aneuska Morillo";
+  const typingElemnet = document.getElementById("typing-text");
+  const typingDelay = 100;
+
+  typeText(text, typingElemnet, typingDelay);
+}
+
+function typeText(text, typingElemnet, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElemnet.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect);
